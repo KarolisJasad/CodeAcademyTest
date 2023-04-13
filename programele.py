2c032f66d1d93a64836973b62d5bbfdae1f4a47b
Karolis Jasadavičius <karolisjasad@gmail.com>
2023-04-13 10:40:29 +0300

    Pridėtas šilta šalta

--- programele.py
+++ programele.py
@@ -1,12 +1,14 @@
 import sys
+import random
 
 print("Sveiki, pasirinkite norimą programą:")
 print("A: Skaičiuotuvas")
 print("B: Žaidimas `Šilta/Šalta`")
+randomskaicius = random.randint(1, 100)
 bandymai = 0
-while bandymai < 2:
+while bandymai < 2:  # Nustatome, kad vartotojas gali spėti tik du kartus
     Pasirinkimas = input("Įveskite norimos programos raidę: ")
-    Pasirinkimas = Pasirinkimas.lower()
+    Pasirinkimas = Pasirinkimas.lower() # Konvertuojame atsakymą į mažąsias raides, del sekančio IF patikrinimo
 
     if Pasirinkimas == "a" or Pasirinkimas == "b":
         print("Jūs pasirinkote", Pasirinkimas)
@@ -15,14 +17,14 @@ while bandymai < 2:
         print("Jūs įvedėte blogą pasirinkimą")
         bandymai += 1
 
-if bandymai == 2 or Pasirinkimas == "b":
+if bandymai == 2: # Patikriname ar vartotojas jau atliko du bandymus
     print("Jūs įvedėte blogą pasirinkimą du kartus, programa bus uždaryta.")
-    sys.exit()
+    sys.exit() # Sustabdome programą, jei per daug bandymų
 
-if Pasirinkimas == "a":
+if Pasirinkimas == "a": # Paleidžiame žaidimą pagal pasirinkimą
     print("Šis skaičiuotuvas priima tik du skaičius, paprašysiu įvesti po vieną atskirai")
     while True:
-        try:
+        try: # Naudojame Try, kad galėtume patikrinti ar skaičius tinkamai įvestas
            skaicius1 = float(input("Įveskite pirmą skaičių: "))
            skaicius2 = float(input("Įveskite antrą skaičių: "))
            operatorius = input("Įveskite norimą operatorių (+, -, /, *, or **): ")
@@ -43,6 +45,26 @@ if Pasirinkimas == "a":
                break
            else:
             print("Neteisingai pasirinktas operatorius, programa perkraunama, įveskite viską iš naujo")
-        except ValueError:
+        except ValueError:  # Patikrina ar vietoj skaičiaus nėra įvestas kitoks simbolis
             print("Neteisingai įvestas skaičius, programa perkraunama, įveskite viską iš naujo")
+elif Pasirinkimas == "b":
+    print("Spėkite skaičių nuo 0 iki 100")
+
+while True:
+    spejimas = input("Įveskite savo spėjima: ")
+    
+    try:
+        spejimas = int(spejimas)
+    except ValueError:
+        print("Blogai įvestas skaičius, įveskite teisingą skaičių.")
+        continue
+    
+    if spejimas == randomskaicius:
+        print("Sveikinu, atspėjote skaičių.")
+        break
+    elif spejimas < randomskaicius:
+        print("Jūsų spėjimas yra žemesnis, bandykite dar kartą")
+    elif spejimas > randomskaicius:
+        print("Jūsų spėjimas didesnis, bandykite dar kartą")
+
         
