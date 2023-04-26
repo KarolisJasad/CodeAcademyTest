0c90a5d8a97555d70e0173065ce7bed4b8624a7d
Karolis Jasadavičius <karolisjasad@gmail.com>
2023-04-26 15:50:13 +0300

    Pakitimai

---
+++ 12_paskaita_logging.py
@@ -0,0 +1,32 @@
+import logging
+# logging.basicConfig(
+#     level=logging.INFO,
+#     filename="istorija.log",
+#     format="%(asctime)s, %(name)s, %(levelname)s, %(message)s"
+# )
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+formatter=logging.Formatter("%(asctime)s, %(name)s, %(levelname)s, %(message)s")
+file_handler = logging.FileHandler('istorija.log')
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+console_handler.setFormatter(logging.Formatter("%(levelname)s, %(message)s"))
+logger.addHandler(file_handler)
+# logger.addHandler(console_handler)
+logger.debug("Perspejimas")
+
+class Produktas():
+    def __init__(self, **kwargs):
+        for kwarg in kwargs:
+            setattr(self, kwarg, kwargs[kwarg])
+            logger.info(f'kuriamo produkto {kwarg}={kwargs[kwarg]}')
+
+daiktas = Produktas(pavadinimas="daiktas")
+kitas = Produktas(pavadinimas="Kitas Daiktas", kaina=1.99)
+
+try:
+    skaicius = 10 / 0
+except ZeroDivisionError as error:
+    logger.exception("Klaida: %s", error)
